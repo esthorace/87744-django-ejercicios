dd6eec2fea34d5463b03a3057206516611310dc7
esthorace <esteban.horacio.acevedo@gmail.com>
2026-09-25 20:19:48 -0300

    wip

---
+++ docs/7-ejercicio.md
@@ -0,0 +1,4 @@
+# Ejercicio
+
+1. Mejorar modelo Servicio
+2. Registrarlo en admin
--- src/servicio/models.py
+++ src/servicio/models.py
@@ -2,5 +2,12 @@ from django.db import models
 
 
 class CategoriaServicio(models.Model):
-    nombre = models.CharField()
-    descripcion = models.TextField()
+    nombre = models.CharField(unique=True)
+    descripcion = models.TextField(blank=True)
+
+    def __str__(self) -> str:
+        return self.nombre
+
+    class Meta:
+        verbose_name = "Categoría de Servicio"
+        verbose_name_plural = "Categorías de Servicio"
